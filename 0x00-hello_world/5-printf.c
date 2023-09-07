ad50837e617a7b94e706193a3b4d68a0c00d38a7
victor daniel <daniel9652985@gmail.com>
2023-09-07 09:45:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program with printf function
+ * Return: 0 (successfully done)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
