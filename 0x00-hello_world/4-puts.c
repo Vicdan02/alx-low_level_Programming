6f4ee6ac3a6e534bbef543a00c93e2529ddb3560
victor daniel <daniel9652985@gmail.com>
2023-09-07 09:28:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program with puts function
+ * Return: 0 (successfully done)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
