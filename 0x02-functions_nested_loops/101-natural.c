247ebae2739e65b9cef5de46864eb1551bde8270
victor daniel <daniel9652985@gmail.com>
2023-09-12 15:42:03 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * main - a program that computes and print the sum of multiple
+ *
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
+}
